72acff21a68d99e6cdc7133c50c7e24d50c2f830
RBianchi-Al <renatalebianchi@gmail.com>
2021-08-31 19:24:53 -0300

    heroku

--- build/server.js
+++
@@ -1,4 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _ = require('./'); var _2 = _interopRequireDefault(_);
-const PORT = process.env.PORT || 5555;
-
-_2.default.listen(PORT, console.log("Server is running 🚀"));
