4172d587d6996254a1b585cad0946bc40a738312
RBianchi-Al <renatalebianchi@gmail.com>
2021-08-31 22:01:04 -0300

    heroku

--- src/server.js
+++ src/server.js
@@ -1,4 +1,15 @@
 import app from './';
+import cors from 'cors'
+
+// middleware
+app.use(cors())
+
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")))
+}
+
 const PORT = process.env.PORT || 3000
+
 
 app.listen(PORT, console.log("Server is running 🚀"));
